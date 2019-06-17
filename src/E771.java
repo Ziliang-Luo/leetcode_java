614336364e11287cc54e11c4830eb3fe67acc873
ziliangluo <aiden551720@gmail.com>
2019-06-17 01:46:21 -0500

    Day1

---
+++ src/E771.java
@@ -0,0 +1,19 @@
+class E771 {
+    public static int numJewelsInStones(String J, String S) {
+        int count = 0;
+        for(int i =0;i<J.length();i++){
+            for(int j =0; j<S.length();j++ ){
+                if (J.charAt(i) == S.charAt(j)){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        String J = "aA";
+        String S = "aaabbb";
+        System.out.println(numJewelsInStones(J,S));
+    }
+}
