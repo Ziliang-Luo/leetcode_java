ecd9c01f7079eb4ae1272f0f5b019548f1c4b8e3
ziliangluo <aiden551720@gmail.com>
2019-07-23 17:07:13 -0500

    Day33

---
+++ src/E1071.java
@@ -0,0 +1,16 @@
+public class E1071 {
+    public String gcdOfStrings(String s1, String s2) {
+        if(s1.length()<s2.length())
+            return gcdOfStrings(s2, s1);
+        if(s2.length() == 0)
+            return s1;
+        if(s1.startsWith(s2)){
+            while(s1.startsWith(s2)){
+                s1 = s1.substring(s2.length());
+            }
+            return gcdOfStrings(s2, s1);
+        } else {
+            return "";
+        }
+    }
+}
---
+++ src/E389.java
@@ -0,0 +1,15 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class E389 {
+    public char findTheDifference(String s, String t) {
+        char c = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            c ^= s.charAt(i);
+        }
+        for (int i = 0; i < t.length(); ++i) {
+            c ^= t.charAt(i);
+        }
+        return c;
+    }
+}
